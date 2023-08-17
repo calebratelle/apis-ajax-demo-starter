857ae078a2c8a8fca3c7b06641dee4d00c388ad4
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-08-17 12:53:19 -0600

    after demo

---
+++ pokemon-async-await.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("get-pokemon");
+
+async function getPokemon(){
+  const numPokemon = document.getElementById("num-pokemon").value;
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${numPokemon}`;
+
+  const {data} = await axios.get(url);
+
+  data.results.forEach(pokemon => {
+    const item = document.createElement("li");
+    item.innerText = pokemon.name;
+    document.getElementById("pokemon-list").appendChild(item);
+})
+}
+
+button.addEventListener("click", getPokemon);
